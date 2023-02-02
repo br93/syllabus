8b69ec5102a63ba03699701a65387fdafdee9fad
br93 <reisalvesb@gmail.com>
2023-02-02 00:13:26 -0300

    Go-Java Sidecar

---
+++ syllabus-settings/src/test/java/com/syllabus/settings/sidecar/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syllabus.settings.sidecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
