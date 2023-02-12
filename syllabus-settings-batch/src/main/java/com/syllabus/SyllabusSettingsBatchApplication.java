f897acd64f746bdafb5f81806e1a78798514d51b
br93 <reisalvesb@gmail.com>
2023-02-11 22:32:53 -0300

    Create Settings-Batch

---
+++ syllabus-settings-batch/src/main/java/com/syllabus/SyllabusSettingsBatchApplication.java
@@ -0,0 +1,13 @@
+package com.syllabus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyllabusSettingsBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyllabusSettingsBatchApplication.class, args);
+	}
+
+}
