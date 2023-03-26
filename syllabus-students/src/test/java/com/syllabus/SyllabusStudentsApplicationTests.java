579a6c6e1feb19a43e546d0e023137a260c26f58
br93 <reisalvesb@gmail.com>
2023-03-26 01:21:28 -0300

    Init Students-MS

--- syllabus-students/src/test/java/com/syllabus/SyllabusStudentsApplicationTests.java
+++ syllabus-students/src/test/java/com/syllabus/SyllabusStudentsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SyllabusUsersApplicationTests {
+class SyllabusStudentsApplicationTests {
 
 	@Test
 	void contextLoads() {
