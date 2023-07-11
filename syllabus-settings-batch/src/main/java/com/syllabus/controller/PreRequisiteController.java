682122b6c1b2dc755ac2003c50b65b53f171a884
br93 <reisalvesb@gmail.com>
2023-07-11 13:40:39 -0300

    Add endpoint csv/pre-requisites

---
+++ syllabus-settings-batch/src/main/java/com/syllabus/controller/PreRequisiteController.java
@@ -0,0 +1,44 @@
+package com.syllabus.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.syllabus.helper.CsvHelper;
+import com.syllabus.message.ResponseMessage;
+import com.syllabus.service.PreRequisiteService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/csv")
+public class PreRequisiteController {
+
+    private final PreRequisiteService preRequisiteService;
+
+    @PostMapping("pre-requisites")
+    public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+        String message = "";
+
+        if (CsvHelper.hasCSVFormat(file)) {
+            try {
+                preRequisiteService.save(file);
+                message = "Uploaded the file successfully: " + file.getOriginalFilename();
+                return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseMessage(message));
+            } catch (Exception ex) {
+                message = "Could not upload the file: " + file.getOriginalFilename();
+                return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+            }
+
+        }
+
+        message = "Please upload a csv file";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message));
+    }
+
+}
