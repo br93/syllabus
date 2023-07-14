e3cb53d836a44a2d7c0fcb7dbfbc3bed66924748
br93 <reisalvesb@gmail.com>
2023-07-14 15:59:45 -0300

    Update student report endpoint

--- syllabus-students/src/main/java/com/syllabus/controller/StudentProfileController.java
+++ syllabus-students/src/main/java/com/syllabus/controller/StudentProfileController.java
@@ -15,7 +15,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("api/v1/users")
+@RequestMapping("api/v1/account/user")
 public class StudentProfileController {
 
     private final StudentService studentService;
