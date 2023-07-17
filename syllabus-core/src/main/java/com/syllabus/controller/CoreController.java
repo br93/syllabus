795d2f2fbd1e13f484bec0dde15c794c54b1d0f1
br93 <reisalvesb@gmail.com>
2023-07-17 16:20:36 -0300

    Converting list to page

--- syllabus-core/src/main/java/com/syllabus/controller/CoreController.java
+++ syllabus-core/src/main/java/com/syllabus/controller/CoreController.java
@@ -1,8 +1,9 @@
 package com.syllabus.controller;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.syllabus.client.settings.CourseProgramResponse;
 import com.syllabus.data.CoreResponseModel;
 import com.syllabus.service.CoreService;
 import com.syllabus.util.CoreMapper;
@@ -25,44 +27,50 @@ public class CoreController {
     private final CoreMapper coreMapper;
 
     @GetMapping("students/{id}/courses-taken")
-    public ResponseEntity<List<CoreResponseModel>> getCoursesTaken(@PathVariable(name = "id") String userId) {
+    public ResponseEntity<Page<CoreResponseModel>> getCoursesTaken(@PageableDefault Pageable pageable, @PathVariable(name = "id") String userId) {
         var coursePrograms = coreService.getCoursesTaken(userId);
+        Page<CourseProgramResponse> pageCoursePrograms = new PageImpl<>(coursePrograms, pageable, coursePrograms.size());
+        
 
         return new ResponseEntity<>(
-                coursePrograms.stream().map(coreMapper::toCoreResponse).collect(Collectors.toList()), HttpStatus.OK);
+                pageCoursePrograms.map(coreMapper::toCoreResponse), HttpStatus.OK);
 
     }
 
     @GetMapping("students/{id}/required-courses")
-    public ResponseEntity<List<CoreResponseModel>> getAllRequiredCourses(@PathVariable(name = "id") String userId) {
+    public ResponseEntity<Page<CoreResponseModel>> getAllRequiredCourses(@PageableDefault Pageable pageable, @PathVariable(name = "id") String userId) {
         var coursePrograms = coreService.getAllRequiredCourses(userId);
+        Page<CourseProgramResponse> pageCoursePrograms = new PageImpl<>(coursePrograms, pageable, coursePrograms.size());
 
         return new ResponseEntity<>(
-                coursePrograms.stream().map(coreMapper::toCoreResponse).collect(Collectors.toList()), HttpStatus.OK);
+                pageCoursePrograms.map(coreMapper::toCoreResponse), HttpStatus.OK);
     }
 
     @GetMapping("students/{id}/required-courses/missing")
-    public ResponseEntity<List<CoreResponseModel>> getMissingRequiredCourses(@PathVariable(name = "id") String userId) {
+    public ResponseEntity<Page<CoreResponseModel>> getMissingRequiredCourses(@PageableDefault Pageable pageable, @PathVariable(name = "id") String userId) {
         var coursePrograms = coreService.getMissingRequiredCourses(userId);
+        Page<CourseProgramResponse> pageCoursePrograms = new PageImpl<>(coursePrograms, pageable, coursePrograms.size());
 
         return new ResponseEntity<>(
-                coursePrograms.stream().map(coreMapper::toCoreResponse).collect(Collectors.toList()), HttpStatus.OK);
+                pageCoursePrograms.map(coreMapper::toCoreResponse), HttpStatus.OK);
     }
 
     @GetMapping("students/{id}/elective-courses")
-    public ResponseEntity<List<CoreResponseModel>> getAllElectiveCourses(@PathVariable(name = "id") String userId) {
+    public ResponseEntity<Page<CoreResponseModel>> getAllElectiveCourses(@PageableDefault Pageable pageable, @PathVariable(name = "id") String userId) {
         var coursePrograms = coreService.getAllElectiveCourses(userId);
+        Page<CourseProgramResponse> pageCoursePrograms = new PageImpl<>(coursePrograms, pageable, coursePrograms.size());
 
         return new ResponseEntity<>(
-                coursePrograms.stream().map(coreMapper::toCoreResponse).collect(Collectors.toList()), HttpStatus.OK);
+                pageCoursePrograms.map(coreMapper::toCoreResponse), HttpStatus.OK);
     }
 
     @GetMapping("students/{id}/elective-courses/missing")
-    public ResponseEntity<List<CoreResponseModel>> getMissingElectiveCourses(@PathVariable(name = "id") String userId) {
+    public ResponseEntity<Page<CoreResponseModel>> getMissingElectiveCourses(@PageableDefault Pageable pageable, @PathVariable(name = "id") String userId) {
         var coursePrograms = coreService.getMissingElectiveCourses(userId);
+        Page<CourseProgramResponse> pageCoursePrograms = new PageImpl<>(coursePrograms, pageable, coursePrograms.size());
 
         return new ResponseEntity<>(
-                coursePrograms.stream().map(coreMapper::toCoreResponse).collect(Collectors.toList()), HttpStatus.OK);
+                pageCoursePrograms.map(coreMapper::toCoreResponse), HttpStatus.OK);
     }
 
 }
