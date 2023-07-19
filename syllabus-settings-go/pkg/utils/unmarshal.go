f3d93be9653b8041a64fed8e9738d2b90057e8d6
br93 <reisalvesb@gmail.com>
2023-07-19 14:02:48 -0300

    Add Unmarshall util

---
+++ syllabus-settings-go/pkg/utils/unmarshal.go
@@ -0,0 +1,33 @@
+package utils
+
+import (
+	"encoding/json"
+
+	"github.com/br93/syllabus/syllabus-settings-go/pkg/models"
+)
+
+func UnmarshalCourses(value string) *[]models.Course {
+
+	var courses []models.Course
+
+	err := json.Unmarshal([]byte(value), &courses)
+	if err != nil {
+		panic(err)
+	}
+
+	return &courses
+}
+
+func UnmarshalCourse(value string) *models.Course {
+
+	var course models.Course
+
+	err := json.Unmarshal([]byte(value), &course)
+	if err != nil {
+		panic(err)
+	}
+
+	course.CourseName = course.CourseName + " CACHED"
+
+	return &course
+}
