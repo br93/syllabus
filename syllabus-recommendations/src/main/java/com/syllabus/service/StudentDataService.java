f8ee9dc5438130387e290236b9ae0ed15b105812
br93 <reisalvesb@gmail.com>
2023-07-27 13:35:41 -0300

    Remove unused import

--- syllabus-recommendations/src/main/java/com/syllabus/service/StudentDataService.java
+++ syllabus-recommendations/src/main/java/com/syllabus/service/StudentDataService.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.syllabus.client.core.CoreClient;
