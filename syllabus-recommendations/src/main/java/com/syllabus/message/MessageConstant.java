46861ae6119e5384c42f88a72d86e2eebfbd4fd2
br93 <reisalvesb@gmail.com>
2023-08-03 22:13:52 -0300

    Add message package

---
+++ syllabus-recommendations/src/main/java/com/syllabus/message/MessageConstant.java
@@ -0,0 +1,11 @@
+package com.syllabus.message;
+
+public class MessageConstant {
+
+    private MessageConstant(){}
+
+    public static final String RECOMMENDATION_NOT_FOUND = "recommendation not found";
+    public static final String SERVICE_UNAVAILABLE = "service unavailable, try again later";
+    public static final String STUDENT_NOT_FOUND = "student data not found";
+    
+}
