925d634f91d76220241357b9c11077d23babdcee
bruno <reisalvesb@gmail.com>
2023-10-25 14:19:11 -0300

    Remove untagged images

--- syllabus-docker/run_docker_compose.sh
+++ syllabus-docker/run_docker_compose.sh
@@ -3,5 +3,6 @@
 compose_files=("docker-network.yml" "docker-db.yml" "docker-kafka.yml" "docker-services.yml")
 
 for file in "${compose_files[@]}"; do
-  docker-compose -f "$file" up -d
+  docker-compose -f "$file" up -d && docker-compose -f "$file" rm -f
+  docker rmi `docker images | grep "<none>" | awk {'print $3'}`
 done
